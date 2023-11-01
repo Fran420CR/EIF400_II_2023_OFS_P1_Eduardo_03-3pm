5398197b3bbf7fe6147a635cf605c55eeaecfbf2
Fran420CR <113930709+Fran420CR@users.noreply.github.com>
2023-11-01 14:06:35 -0600

    0.6

--- test.ofs/test_1.ofs.ts
+++ test.ofs/test_1.ofs.ts
@@ -1,6 +1,12 @@
-import {Stream} from './stream'
+import { Stream } from './stream';
 
 const nats = Stream.iterate(0, (n) => n + 1);
 const even = nats.filter((n) => n % 2 === 0);
 const evenLessThanEleven = even.filter((n) => n < 11);
-evenLessThanEleven.toList().forEach((n) => console.log(n));
+
+const evenNumbersLessThanEleven = evenLessThanEleven.toList();
+
+//evenLessThanEleven.toList().forEach((n) => console.log(n));
+for (const number of evenNumbersLessThanEleven) {
+  console.log(number);
+}
--- test.ofs/test_2.ofs.ts
+++ test.ofs/test_2.ofs.ts
@@ -1,8 +1,12 @@
-import {Stream} from './stream'
+import { Stream } from './stream';
 
 const nats = Stream.iterate(0, (n) => n + 1);
 const even = nats.filter((n) => n % 2 === 0);
 const evenGreaterThanTen = even.filter((n) => n > 10);
-const onlyFiveAfterTen = evenGreaterThanTen.toList().slice(0, 5); // Cortamos los primeros 5 elementos
+const onlyFiveAfterTen = evenGreaterThanTen.toList().slice(0, 5);
 
-onlyFiveAfterTen.forEach((n) => console.log(n));
+//onlyFiveAfterTen.forEach((n) => console.log(n));
+for (const number of onlyFiveAfterTen) {
+  console.log(number);
+}
+
